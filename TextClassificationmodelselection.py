5a1c1266ceeb12efb928bf6ba8d585c00b296e36
NAILI KAMEL <kamel.naili@sagemcom.com>
2019-08-04 19:45:36 +0100

    Add BOW with keras & Doc2vec and Logistic Regression Current implemented algorithmes :
            - Naive Bayes classifier for multinomial models¶
            - Linear support vector machine¶
            - Logistic regression¶
            - Doc2vec and Logistic Regression¶
            - BOW with keras¶

--- TextClassificationmodelselection.py
+++ TextClassificationmodelselection.py
@@ -13,7 +13,11 @@ import re
 from bs4 import BeautifulSoup
 from IPython import get_ipython
 from gensim.models import Word2Vec
+import pandas as pd
+import numpy as np
 from tqdm import tqdm
+
+
 import itertools
 import os
 #% matplotlib
@@ -77,6 +81,7 @@ X = df.post
 y = df.tags
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+#Naive Bayes classifier for multinomial models¶
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -95,6 +100,8 @@ y_pred = nb.predict(X_test)
 print('accuracy %s' % accuracy_score(y_pred, y_test))
 print(classification_report(y_test, y_pred, target_names=my_tags))
 
+#Linear support vector machine¶
+
 from sklearn.linear_model import SGDClassifier
 
 sgd = Pipeline([('vect', CountVectorizer()),
@@ -110,6 +117,8 @@ y_pred = sgd.predict(X_test)
 print('accuracy %s' % accuracy_score(y_pred, y_test))
 print(classification_report(y_test, y_pred, target_names=my_tags))
 
+#Logistic regression¶
+
 from sklearn.linear_model import LogisticRegression
 
 logreg = Pipeline([('vect', CountVectorizer()),
@@ -124,6 +133,157 @@ y_pred = logreg.predict(X_test)
 
 print('accuracy %s' % accuracy_score(y_pred, y_test))
 y_pred = logreg.predict(X_test)
+
+print('accuracy %s' % accuracy_score(y_pred, y_test))
+print(classification_report(y_test, y_pred,target_names=my_tags))
+
+
+#BOW with keras¶
+
+import itertools
+ipy = get_ipython()
+if ipy is not None:
+    ipy.run_line_magic('matplotlib', 'inline')
+
+import os
+
+#%matplotlib inline
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.metrics import confusion_matrix
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.preprocessing import text, sequence
+from keras import utils
+
+train_size = int(len(df) * .7)
+print ("Train size: %d" % train_size)
+print ("Test size: %d" % (len(df) - train_size))
+
+train_posts = df['post'][:train_size]
+train_tags = df['tags'][:train_size]
+
+test_posts = df['post'][train_size:]
+test_tags = df['tags'][train_size:]
+
+max_words = 1000
+tokenize = text.Tokenizer(num_words=max_words, char_level=False)
+
+tokenize.fit_on_texts(train_posts) # only fit on train
+x_train = tokenize.texts_to_matrix(train_posts)
+x_test = tokenize.texts_to_matrix(test_posts)
+
+encoder = LabelEncoder()
+encoder.fit(train_tags)
+y_train = encoder.transform(train_tags)
+y_test = encoder.transform(test_tags)
+
+num_classes = np.max(y_train) + 1
+y_train = utils.to_categorical(y_train, num_classes)
+y_test = utils.to_categorical(y_test, num_classes)
+
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+print('y_train shape:', y_train.shape)
+print('y_test shape:', y_test.shape)
+
+batch_size = 32
+epochs = 2
+
+# Build the model
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_split=0.1)
+
+score = model.evaluate(x_test, y_test,
+                       batch_size=batch_size, verbose=1)
+print('Test accuracy:', score[1])
+
+# Doc2vec and Logistic Regression
+from tqdm import tqdm
+tqdm.pandas(desc="progress-bar")
+#tqdm.pandas(desc="my bar!")
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim import utils
+from gensim.models import doc2vec, keyedvectors
+from sklearn import utils
+import gensim
+#from gensim.models.doc2vec import TaggedDocument
+
+import re
+
+def label_sentences(corpus, label_type):
+    """
+    Gensim's Doc2Vec implementation requires each document/paragraph to have a label associated with it.
+    We do this by using the TaggedDocument method. The format will be "TRAIN_i" or "TEST_i" where "i" is
+    a dummy index of the post.
+    """
+    labeled = []
+    for i, v in enumerate(corpus):
+        label = label_type + '_' + str(i)
+        labeled.append(doc2vec.TaggedDocument(v.split(), [label]))
+    return labeled
+
+X_train, X_test, y_train, y_test = train_test_split(df.post, df.tags, random_state=0, test_size=0.3)
+X_train = label_sentences(X_train, 'Train')
+X_test = label_sentences(X_test, 'Test')
+all_data = X_train + X_test
+
+all_data[:2]
+
+model_dbow = Doc2Vec(dm=0, vector_size=300, negative=5, min_count=1, alpha=0.065, min_alpha=0.065)
+model_dbow.build_vocab([x for x in tqdm(all_data)])
+
+for epoch in range(30):
+    model_dbow.train(utils.shuffle([x for x in tqdm(all_data)]), total_examples=len(all_data), epochs=1)
+    model_dbow.alpha -= 0.002
+    model_dbow.min_alpha = model_dbow.alpha
+
+
+
+def get_vectors(model, corpus_size, vectors_size, vectors_type):
+    """
+    Get vectors from trained doc2vec model
+    :param doc2vec_model: Trained Doc2Vec model
+    :param corpus_size: Size of the data
+    :param vectors_size: Size of the embedding vectors
+    :param vectors_type: Training or Testing vectors
+    :return: list of vectors
+    """
+    vectors = np.zeros((corpus_size, vectors_size))
+    for i in range(0, corpus_size):
+        prefix = vectors_type + '_' + str(i)
+        vectors[i] = model.docvecs[prefix]
+    return vectors
+
+train_vectors_dbow = get_vectors(model_dbow, len(X_train), 300, 'Train')
+test_vectors_dbow = get_vectors(model_dbow, len(X_test), 300, 'Test')
+
+logreg = LogisticRegression(n_jobs=1, C=1e5)
+logreg.fit(train_vectors_dbow, y_train)
+
+logreg = logreg.fit(train_vectors_dbow, y_train)
+y_pred = logreg.predict(test_vectors_dbow)
 
 print('accuracy %s' % accuracy_score(y_pred, y_test))
 print(classification_report(y_test, y_pred,target_names=my_tags))
